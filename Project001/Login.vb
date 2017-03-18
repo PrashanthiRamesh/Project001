6febe4ce3ced33645ae7a2828d05c269a5ecdb80
Prashanthi Ramesh <rvprashanthi@gmail.com>
2017-03-19 04:07:15 +0530

    Login auth

--- Project001/Login.vb
+++ Project001/Login.vb
@@ -11,6 +11,26 @@ Public Class FormLogin
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim constring As String = GlobalVariables.connectionString
+        Using myconnection As New OleDbConnection(constring)
+            myconnection.Open()
+            Dim sqlQry As String = "SELECT * FROM [user] WHERE ([name]=@name AND [password]=@password)"
+            Using cmd As New OleDbCommand(sqlQry, myconnection)
+                cmd.Parameters.AddWithValue("@name", TextBox1.Text)
+                cmd.Parameters.AddWithValue("@password", TextBox2.Text)
+                Dim sdr As OleDbDataReader = cmd.ExecuteReader()
+                ' If the record can be queried, it means passing verification, then open another form.   
+                If (sdr.Read() = True) Then
+                    MessageBox.Show("Logged into Botique Management System")
+                    Dim homeForm As New Home
+                    homeForm.Show()
+                    Me.Hide()
+                Else
+                    MessageBox.Show("Invalid name or password!")
+                End If
+            End Using
+        End Using
+
 
     End Sub
 
